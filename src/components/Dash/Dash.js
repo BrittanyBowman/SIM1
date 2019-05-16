9d48db2de19bc23768948ee6fe99db9ab2d6f63e
Brittany Bowman <britpetty@gmail.com>
2019-05-16 10:49:49 -0600

    Second Commit

---
+++ src/components/Dash/Dash.js
@@ -0,0 +1,11 @@
+//Create a simple class component in the Dashboard.
+
+import React, { Component } from "react";
+
+export class Dash extends Component {
+  render() {
+    return <div>Dash</div>;
+  }
+}
+
+export default Dash;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,11 @@
+//Create a simple class component in the Form.
+
+import React, { Component } from "react";
+
+export class Form extends Component {
+  render() {
+    return <div>Form</div>;
+  }
+}
+
+export default Form;
